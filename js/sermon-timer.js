39d3da3e48c8c51d9762f36885422cb560e0c4ca
Stephen Webster <stephen.webster09@gmail.com>
2016-04-10 16:55:50 -0500

    Just messing around
    asdfasd

---
+++ js/sermon-timer.js
@@ -0,0 +1,14 @@
+// Gonna try using JAVASCRIPT!!
+
+function performcountdown() {
+//grab inputs
+var h = document.getElementById('time-input-hours').value
+var m = document.getElementById('time-input-min').value
+var s = document.getElementById('time-input-sec').value
+
+//Convert to miliseconds
+var countdown = ((h * 60 * 60) + (m * 60) + s) * 1000
+
+
+
+}
